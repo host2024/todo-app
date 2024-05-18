542c07b43a45df499daead5c4a791c45be2f50bd
host2024 <youwoon5520@gmail.com>
2024-05-18 19:36:44 +0900

    완성

--- todo/src/components/todo/TodoContainer.jsx
+++ todo/src/components/todo/TodoContainer.jsx
@@ -3,20 +3,7 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const TodoContainer = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            title: '할 일 1',
-            content: '할 일 1 내용',
-            isDone: false,
-        },
-        {
-            id: 2,
-            title: '할 일 2',
-            content: '할 일 2 내용',
-            isDone: true,
-        },
-    ]);
+    const [todos, setTodos] = useState([]);
 
     const workingTodos = todos.filter((todo) => !todo.isDone);
     const doneTodos = todos.filter((todo) => todo.isDone);
